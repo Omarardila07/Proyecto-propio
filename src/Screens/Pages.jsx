cbbb8356fb0ae601544f34adb4af01eb4a779097
Omarardila <ardilaomar848@gmail.com>
2024-02-29 12:54:47 -0500

    Proyecto Propio

--- src/Screens/Pages.jsx
+++ src/Screens/Pages.jsx
@@ -1,76 +1,198 @@
-import React, { useState } from 'react';
-import { firestore } from '../Firebase/config'; // Importa firestore desde tu archivo de configuración de Firebase
+import { useState, useEffect } from "react";
+import img2 from "../Img/img.png";
+import escudo from "../Img/escudo.png";
+import img02 from "../Img/02.png";
+import img01 from "../Img/01.png";
+import img03 from "../Img/03.png";
+import imgC01 from "../Img/C01.png";
+import imgC02 from "../Img/C02.png";
+import imgC03 from "../Img/C03.png";
+
 
 const Pages = () => {
-  const [name, setName] = useState('');
-  const [grade, setGrade] = useState('');
-  const [comment, setComment] = useState('');
-  const [submittedData, setSubmittedData] = useState(null);
+  const [popupOpen, setPopupOpen] = useState(false);
+  const [selectedCandidate, setSelectedCandidate] = useState(null);
+  const [votes, setVotes] = useState(() => {
+    const storedVotes = localStorage.getItem("votes");
+    return storedVotes
+      ? JSON.parse(storedVotes)
+      : {
+          "Candidato 01": 0,
+          "Candidato 02": 0,
+          "Candidato 03": 0,
+          "voto en blanco": 0,
+          "Candidato C04": 0,
+          "Candidato C05": 0,
+          "Candidato C06": 0,
+          "Cvoto en blanco": 0,
+        };
+  });
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    
-    try {
-      // Guarda los datos en Firestore
-      await firestore.collection('Comentarios').add({
-        name: name,
-        grade: grade,
-        comment: comment,
-        timestamp: new Date() // Agrega un campo de marca de tiempo con la fecha actual
-      });
-  
-      // Actualiza el estado con los datos enviados
-      setSubmittedData({ name, grade, comment });
-    } catch (error) {
-      alert('Error al enviar el formulario:', error);
-      // Agrega un mensaje de error o realiza alguna acción adicional en caso de error
-    }
+  useEffect(() => {
+    localStorage.setItem("votes", JSON.stringify(votes));
+  }, [votes]);
+
+  const openPopup = () => {
+    setPopupOpen(true);
+  };
+
+  const CerraPopUp = () => {
+    setPopupOpen(false);
+  }
+
+
+  const handleVote = (candidateName) => {
+    setSelectedCandidate(candidateName);
+    setVotes((prevVotes) => ({
+      ...prevVotes,
+      [candidateName]: prevVotes[candidateName] + 1,
+    }));
   };
 
   return (
-    <div>
-      <h2 className='text-white'>Formulario</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name" className='text-white'>Nombre:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
+    <>
+      <div className="flex justify-center items-center self-center text-center w-[80%] mt-[10%] gap-3 bg-green-600 p-3 rounded-3xl">
+        <div className="w-[45%] text-center font-sans">
+          <div>
+            <h1 className="text-white font-semibold text-[45px]">
+              Hola, ¿por quién vas a votar en estas elecciones 2024 en el Cesar
+              Pompeyo Mendoza Hinojoza?
+            </h1>
+          </div>
+          <br />
+          <button
+            className="p-3 text-[25px] bg-green-900 text-white font-semibold rounded-2xl hover:bg-white hover:text-green-600 transition duration-500 ease-in-out"
+            onClick={openPopup}
+          >
+            Vota aquí
+          </button>
         </div>
-        <div>
-          <label htmlFor="grade" className='text-white'>Grado:</label>
-          <input
-            type="text"
-            id="grade"
-            value={grade}
-            onChange={(e) => setGrade(e.target.value)}
-            required
-          />
+        <div className="w-[300px] h-[510px]">
+          <img src={escudo} alt="" className="" />
         </div>
-        <div>
-          <label htmlFor="comment" className='text-white'>Comentario:</label>
-          <textarea
-            id="comment"
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" className='text-white'>Enviar</button>
-      </form>
+      </div>
 
-      {submittedData && (
-        <div>
-          <h2 className='text-white'>Datos enviados:</h2>
-          <p className='text-white'>Nombre: {submittedData.name}</p>
-          <p className='text-white'>Grado: {submittedData.grade}</p>
-          <p className='text-white'>Comentario: {submittedData.comment}</p>
+      {popupOpen && (
+      
+        <div className="fixed inset-0 flex justify-center itemsm-center bg-black bg-opacity-50 ">
+          <div className="bg-green-500 p-8 rounded-3xl">
+            <h1 className="text-center text-xl font-semibold mb-4 text-white ">
+              Elige a tu candidato de personeria 2024
+            </h1>
+            <div className="grid grid-cols-4 gap-4 ">
+              <Card
+                imageSrc={img01}
+                name="Candidato 01"
+                namee="Candidato"
+                name1="01"
+                isSelected={selectedCandidate === "Candidato 01"}
+                onVote={handleVote}
+                votes={votes["Candidato 01"]}
+              />
+              <Card
+                imageSrc={img02}
+                name="Candidato 02"
+                namee="Candidato"
+                name1="02"
+                isSelected={selectedCandidate === "Candidato 02"}
+                onVote={handleVote}
+                votes={votes["Candidato 02"]}
+              />
+              <Card
+                imageSrc={img03}
+                name="Candidato 03"
+                namee="Candidato"
+                name1="03"
+                isSelected={selectedCandidate === "Candidato 03"}
+                onVote={handleVote}
+                votes={votes["Candidato 03"]}
+              />
+              <Card
+                imageSrc={img2}
+                name="voto en blanco"
+                namee="Voto en blanco"
+                isSelected={selectedCandidate === "voto en blanco"}
+                onVote={handleVote}
+                votes={votes["voto en blanco"]}
+              />
+            </div>
+            <h1 className="text-center text-xl font-semibold mb-4 text-white mt-4 ">
+              Elige a tu candidato de Contraloria 2024
+            </h1>
+            <div className="grid grid-cols-4 gap-4 ">
+              <Card
+                imageSrc={imgC01}
+                name="Candidato C04"
+                namee="Candidato"
+                name1="01"
+                isSelected={selectedCandidate === "Candidato C04"}
+                onVote={handleVote}
+                votes={votes["Candidato C04"]}
+              />
+              <Card
+                imageSrc={imgC02}
+                name="Candidato C05"
+                namee="Candidato"
+                name1="02"
+                isSelected={selectedCandidate === "Candidato C05"}
+                onVote={handleVote}
+                votes={votes["Candidato C05"]}
+              />
+              <Card
+                imageSrc={imgC03}
+                name="Candidato C06"
+                namee="Candidato"
+                name1="03"
+                isSelected={selectedCandidate === "Candidato C06"}
+                onVote={handleVote}
+                votes={votes["Candidato C06"]}
+              />
+              <Card
+                imageSrc={img2}
+                name="Cvoto en blanco"
+                namee="Voto en blanco"
+                isSelected={selectedCandidate === "Cvoto en blanco"}
+                onVote={handleVote}
+                votes={votes["Cvoto en blanco"]}
+              />
+            </div>
+            <div className="flex justify-center self-center items-center mt-8">
+              <button className="p-3 text-[25px] bg-green-900 text-white font-semibold rounded-2xl hover:bg-white hover:text-green-600 transition duration-500 ease-in-out" onClick={CerraPopUp}> Finalizar fomulario </button>
+            </div>
+          </div>
         </div>
+        
       )}
+
+    </>
+  );
+};
+
+
+
+const Card = ({ imageSrc, name, isSelected, onVote, name1, votes, namee }) => {
+  return (
+    <div
+      className={`bg-gray-100 p-4  gap-3 rounded-3xl flex justify-center self-center text-center ${
+        isSelected ? "opacity-50" : ""
+      }`}
+    >
+      <div className="w-[200px]">
+        <img src={imageSrc} alt={name} className=" rounded-3xl" />
+      </div>
+      <div className="self-center">
+        <h2 className="font-semibold text-lg">{namee}</h2>
+        <h2 className="font-semibold text-[70px]">{name1}</h2>
+        <button
+          className="mt-4 bg-green-500 text-white font-semibold py-2 px-4 rounded"
+          onClick={() => onVote(name)}
+        >
+          Votar
+        </button>
+        <br />
+        <br />
+        <h1 className="text-lg font-semibold">Votos = {votes}</h1>
+      </div>
     </div>
   );
 };
